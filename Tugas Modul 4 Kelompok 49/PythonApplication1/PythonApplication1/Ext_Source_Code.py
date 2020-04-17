2da53d504e8afbe78ea336708d1db67b48faae0c
SMAMHTN <aldismartkid@gmail.com>
2020-04-17 14:11:55 +0700

    Update Ext Source Code
    Menambahkan welcome dan daftar pembelian serta inisialisasi

--- Tugas Modul 4 Kelompok 49/PythonApplication1/PythonApplication1/Ext_Source_Code.py
+++ Tugas Modul 4 Kelompok 49/PythonApplication1/PythonApplication1/Ext_Source_Code.py
@@ -1,1 +1,20 @@
+class main:
+    def __init__ (self, nama, email, HP):
+        self.nama=nama
+        self.email=email
+        self.HP=HP
+        self.logam=["Emas","Logam"]
+        self.harga=[732600,216044]
 
+    def welcome (self):
+        print(f"Selamat datang, {self.nama} ! Selamat bertransaksi di Mesin Toko Emas Otomatis Kelompok 49!")
+        print("Mesin ini akan melakukan bunga harga sebagai berikut:")
+        print("Penjualan\t:5%")
+        print("Pembelian\t:10%")
+
+    def pembelian_fitur(self):
+        print("Masukkan jenis logam yang ingin anda beli")
+        count=1
+        for x in self.logam :
+            print (count,".",x)
+            count=count+1
